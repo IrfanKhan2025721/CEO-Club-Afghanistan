bef8853a4307ab9b7c6ee95910fe33232264d263
irfanKhan2025721 <irfanahmadzai1900@gmail.com>
2026-02-10 15:41:35 +0430

    removed unwanted data

--- src/Components/MembersPage/SingleMemberPage/SingleMemberStory/MemberStory.jsx
+++ src/Components/MembersPage/SingleMemberPage/SingleMemberStory/MemberStory.jsx
@@ -55,7 +55,7 @@ export default function MemberStory() {
                 icon={FaSquareXTwitter}
                 text="Twitter"
                 href={member.xlink}
-                bg="#000000"
+                bg="#0A66C2"
               />
             </div>
           )}
