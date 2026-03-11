60c7d6f7b0205c1964f826d19b90a15d43e1e671
irfanKhan2025721 <irfanahmadzai1900@gmail.com>
2026-03-11 10:22:55 +0430

    feat: add PWA support with vite-plugin-pwa and update package.json

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "CEO Club Afghanistan",
+        short_name: "CEO Club",
+        description:
+          "A platform for CEOs in Afghanistan to connect, share insights, and collaborate on business opportunities.",
+        theme_color: "#BB7D1E",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "/LOGO.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/LOGO.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
